1ad7e45124a3a3c0c381745345072109f9476d05
NehalSoni765 <nebakso16@gmail.com>
2023-10-01 19:00:35 +0530

    explain hooks with reconsilation algoritham

--- module4-swiggy/src/components/Body.js
+++ module4-swiggy/src/components/Body.js
@@ -1,17 +1,41 @@
-import { resList } from "../utils/resList"
-import RestaurantCard from "./RestaurantCard"
+import { useState } from "react";
+import { resList } from "../utils/resList";
+import RestaurantCard from "./RestaurantCard";
 
-const Body = ()=>{
-    return(
-        <div className="body">
-            <div className="search">Search</div>
-            <div className="res-container">
-                {resList.map(restaurant=>{
-                    return <RestaurantCard resData={restaurant} key={restaurant.data.id} />
-                })}
-            </div>
-        </div>
-    )
-}
+const Body = () => {
+  //state variable  = super powerful variable
+  const [state, setstate] = useState(resList);
 
-export default Body
+//   //destructuring 
+//   const arr = useState(resList);
+//   const [listItems,setListItems] = arr;
+//   const list = arr[0];
+//   const setList = arr[1];
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log("button clicked");
+            setstate((state) =>
+              state.filter((restaurant) => restaurant.data.avgRating > 4)
+            );
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+      <div className="res-container">
+        {state.map((restaurant) => {
+          return (
+            <RestaurantCard resData={restaurant} key={restaurant.data.id} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
